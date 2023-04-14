ebcc4f8257befd024f1d402a3cd02772d1845e78
Thomas Inman <tinmanc35@gmail.com>
2023-04-13 21:52:07 -0700

    work on lab 1

---
+++ Testing/CarriageReturn&LineFeed.py
@@ -0,0 +1,2 @@
+print("This \n is \n my \n sample.")
+
---
+++ Testing/print_statements.py
@@ -0,0 +1,8 @@
+print     ("Hello World!")
+
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+
+print("Hi! This is a double quote mark: " We use it for printing.")
+
+print("Hi! This is a double quote mark: \" We use it for printing.")
